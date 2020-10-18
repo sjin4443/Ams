723b209b32d18cb58ffa78bc375aca33e22904d5
sjin4443 <71583444+sjin4443@users.noreply.github.com>
2020-10-19 00:46:04 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    $(".cloud1").click(function(){
+        $(".cloud1_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".cloud2").click(function(){
+        $(".cloud2_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".cloud3").click(function(){
+        $(".cloud3_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".tree1").click(function(){
+        $(".tree1_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".tree2").click(function(){
+        $(".tree2_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".bike").click(function(){
+        $(".bike_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+    $(".boat").click(function(){
+        $(".boat_speak").css({
+        'display':'inline-block'
+            })
+    })
+
+
+})
